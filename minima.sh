0017bed29c09bf2c55f54c5b9bd5b7effc42cb74
bazideneme <119060310+bazideneme@users.noreply.github.com>
2022-11-25 18:14:36 +0300

    Update minima.sh

--- minima.sh
+++ minima.sh
@@ -1,2 +1,2 @@
-docker run -d -e minima_mdspassword=123 -e minima_desktop=true -v C:\Users\INSERTUSERNAME\minimadocker9001:/home/minima/data -p 9001-9004:9001-9004 --restart unless-stopped --name minima9001 minimaglobal/minima:latest
+sudo wget -O minima_remove.sh https://raw.githubusercontent.com/minima-global/Minima/master/scripts/minima_remove.sh && chmod +x minima_remove.sh && sudo ./minima_remove.sh -p 9001 -x
 
