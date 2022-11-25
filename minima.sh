10bb703caeeefda8d3bef323f8a1705cac515a15
bazideneme <119060310+bazideneme@users.noreply.github.com>
2022-11-25 18:22:45 +0300

    Update minima.sh

--- minima.sh
+++ minima.sh
@@ -1,2 +1,5 @@
 sudo wget -O minima_remove.sh https://raw.githubusercontent.com/minima-global/Minima/master/scripts/minima_remove.sh && chmod +x minima_remove.sh && sudo ./minima_remove.sh -p 9001 -x
 
+read -p "Incentive ıd girin: " INCENTIVE_ID 
+
+echo -e "Ivent id: ${CYAN}$INCENTIVE_ID${NC}"
